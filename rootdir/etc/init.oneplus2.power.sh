6797d2b56a334cb6354094bf98ab9380620c0c00
hardwareadictos <hardwareadictos@gmail.com>
2018-02-01 21:38:10 +0100

    oneplus2: Update Hotplug * Keep always 2 Big Cores as on OxygenOS * Dont Hotplug Little Cluster

--- rootdir/etc/init.oneplus2.power.sh
+++ rootdir/etc/init.oneplus2.power.sh
@@ -107,7 +107,7 @@ write /sys/module/cpu_boost/parameters/input_boost_ms 40
 
 # Configure core_ctl module parameters
 write /sys/devices/system/cpu/cpu4/core_ctl/max_cpus 4
-write /sys/devices/system/cpu/cpu4/core_ctl/min_cpus 1
+write /sys/devices/system/cpu/cpu4/core_ctl/min_cpus 2
 write /sys/devices/system/cpu/cpu4/core_ctl/busy_up_thres 60
 write /sys/devices/system/cpu/cpu4/core_ctl/busy_down_thres 30
 write /sys/devices/system/cpu/cpu4/core_ctl/offline_delay_ms  100
@@ -115,7 +115,7 @@ write /sys/devices/system/cpu/cpu4/core_ctl/task_thres 4
 write /sys/devices/system/cpu/cpu0/core_ctl/not_preferred 0
 write /sys/devices/system/cpu/cpu4/core_ctl/is_big_cluster 1
 write /sys/devices/system/cpu/cpu0/core_ctl/max_cpus 4
-write /sys/devices/system/cpu/cpu0/core_ctl/min_cpus 0
+write /sys/devices/system/cpu/cpu0/core_ctl/min_cpus 4
 write /sys/devices/system/cpu/cpu0/core_ctl/busy_up_thres 20
 write /sys/devices/system/cpu/cpu0/core_ctl/busy_down_thres 5
 write /sys/devices/system/cpu/cpu0/core_ctl/offline_delay_ms 100
