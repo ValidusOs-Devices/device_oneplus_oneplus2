OzzysCmAcc/android_device_oneplus_oneplus2

oneplus2: Move to fs config gen
Change-Id: Iacba15145adb4176ac8c88df3dd4b48d139f1365

BoardConfig.mk
@@ -124,7 +124,7 @@ BOARD_USES_QCNE := true
 ENABLE_CPUSETS := true
 
 # Filesystem
-TARGET_ANDROID_FILESYSTEM_CONFIG_H := $(PLATFORM_PATH)/android_filesystem_config.h
+TARGET_FS_CONFIG_GEN := $(VENDOR_PATH)/config.fs
 
 # GPS
 TARGET_NO_RPC := true

ValidusOs-Devices/device_oneplus_oneplus2

BoardConfig.mk
@@ -117,7 +117,7 @@ ENABLE_CPUSETS := true
 ENABLE_SCHED_BOOST := true
 
 # Filesystem
-TARGET_ANDROID_FILESYSTEM_CONFIG_H := $(PLATFORM_PATH)/android_filesystem_config.h
+TARGET_FS_CONFIG_GEN := $(PLATFORM_PATH)/config.fs
 
 # GPS
 TARGET_NO_RPC := true