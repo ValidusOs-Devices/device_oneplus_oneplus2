38c91dcfd786e7a81b28eb68b117cebd20b4f1b5
alliancesparx <alliancesparx@gmail.com>
2019-01-27 18:09:40 +0000

    op2: Import DeviceHandler from oppo_common
    Taken from 9.0 branch here: https://github.com/GZOSP-Devices/device_oppo_common

--- device.mk
+++ device.mk
@@ -133,6 +133,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.hwui.text_small_cache_height=1024 \
     ro.hwui.text_large_cache_width=2048 \
     ro.hwui.text_large_cache_height=1024
+
+# DevicHandler
+PRODUCT_PACKAGES += \
+	DeviceHandler
     
 # DRM
 PRODUCT_PACKAGES += \
