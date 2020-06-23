LineageOS/android_device_oneplus_oneplus2

oneplus2: camera: Improve min frame rate hack
AFAIK, there are apps that request 15fps min frame rate; this is
deliberate and we shouldn't change its default behaviour. But anything
that sets the min fps the same as the max fps definitely should just
stop.

Change-Id: I5c3a31241009ab79edba4191910ad99d2db30f71

camera/QCamera2/HAL/QCameraParameters.cpp
@@ -5730,6 +5730,13 @@ int32_t QCameraParameters::setPreviewFpsRange(int min_fps,
     property_get("persist.debug.set.fixedfps", value, "0");
     fixedFpsValue = atoi(value);
 
+    // Don't allow function callers to request min fps same as max fps
+    // I mean SnapdragonCamera.
+    if (max_fps >= 24000 && min_fps == max_fps) {
+        CDBG_H("min_fps %d same as max_fps %d, setting min_fps to 7000", min_fps, max_fps);
+        min_fps = 7000;
+    }
+
     CDBG("%s: E minFps = %d, maxFps = %d , vid minFps = %d, vid maxFps = %d",
                 __func__, min_fps, max_fps, vid_min_fps, vid_max_fps);
 
@@ -5738,9 +5745,8 @@ int32_t QCameraParameters::setPreviewFpsRange(int min_fps,
       max_fps = (int)fixedFpsValue*1000;
     }
     snprintf(str, sizeof(str), "%d,%d", min_fps, max_fps);
-    CDBG_HIGH("%s: Actual preview fps range %s", __func__, str);
-    updateParamEntry(KEY_PREVIEW_FPS_RANGE, "7000,30000");
-    CDBG_HIGH("%s: Setting preview fps range %s", __func__, "7000,30000");
+    CDBG_HIGH("%s: Setting preview fps range %s", __func__, str);
+    updateParamEntry(KEY_PREVIEW_FPS_RANGE, str);
     cam_fps_range_t fps_range;
     memset(&fps_range, 0x00, sizeof(cam_fps_range_t));
     fps_range.min_fps = (float)min_fps / 1000.0f;

ValidusOs-Devices/device_oneplus_oneplus2

camera/QCamera2/HAL/QCameraParameters.cpp
@@ -5730,6 +5730,13 @@ int32_t QCameraParameters::setPreviewFpsRange(int min_fps,
     property_get("persist.debug.set.fixedfps", value, "0");
     fixedFpsValue = atoi(value);
 
+    // Don't allow function callers to request min fps same as max fps
+    // I mean SnapdragonCamera.
+    if (max_fps >= 24000 && min_fps == max_fps) {
+        CDBG_HIGH("min_fps %d same as max_fps %d, setting min_fps to 7000", min_fps, max_fps);
+        min_fps = 7000;
+    }
+
     CDBG("%s: E minFps = %d, maxFps = %d , vid minFps = %d, vid maxFps = %d",
                 __func__, min_fps, max_fps, vid_min_fps, vid_max_fps);
 
@@ -5738,9 +5745,8 @@ int32_t QCameraParameters::setPreviewFpsRange(int min_fps,
       max_fps = (int)fixedFpsValue*1000;
     }
     snprintf(str, sizeof(str), "%d,%d", min_fps, max_fps);
-    CDBG_HIGH("%s: Actual preview fps range %s", __func__, str);
-    updateParamEntry(KEY_PREVIEW_FPS_RANGE, "7000,30000");
-    CDBG_HIGH("%s: Setting preview fps range %s", __func__, "7000,30000");
+    CDBG_HIGH("%s: Setting preview fps range %s", __func__, str);
+    updateParamEntry(KEY_PREVIEW_FPS_RANGE, str);
     cam_fps_range_t fps_range;
     memset(&fps_range, 0x00, sizeof(cam_fps_range_t));
     fps_range.min_fps = (float)min_fps / 1000.0f;